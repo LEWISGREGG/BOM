74fc3eb500a13a7271115f378acc980176488b79
SERG <40707060+LEWISGREGG@users.noreply.github.com>
2024-03-22 20:48:53 -0400

    Add files via upload

--- components/SignOut.js
+++ components/SignOut.js
@@ -3,6 +3,7 @@ import { auth } from '.././firebase'
 import Image from "next/image";
 import { useRouter } from "next/router";
 
+
 function SignOut() {
     const router = useRouter();
     return (
@@ -10,8 +11,18 @@ function SignOut() {
     <div onClick={() => router.push('/') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
 <Image src="https://uploads.turbologo.com/uploads/design/hq_preview_image/3055644/draw_svg20210625-19886-1te2nks.svg.png" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded '/>
 </div>
+
 <div onClick={() => router.push('/TerminalCount') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
 <Image src="https://uploads.turbologo.com/uploads/design/hq_preview_image/3055208/draw_svg20210713-19572-1ba8efu.svg.png" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
+</div>
+
+<div onClick={() => router.push('/Cable') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
+<Image src="https://www.svgrepo.com/show/420135/cable-device-electronic.svg" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
+</div>
+
+
+<div onClick={() => router.push('/Hoa') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
+<Image src="https://www.svgrepo.com/show/420104/device-electronic-machine-21.svg" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
 </div>
             <button className='text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900' 
             objectposition="right" onClick={() => auth.signOut()}>Sign Out</button>
