6e67531359a35d1c8bfd5d75daaa200db3d93fa1
SERG <40707060+LEWISGREGG@users.noreply.github.com>
2024-03-22 20:50:04 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
   images:{
-    domains:['lh3.googleusercontent.com', 'https://www.jsonkeeper.com', "www.pexels.com", "images.pexels.com", "uploads.turbologo.com" ]
+    domains:['lh3.googleusercontent.com', 'https://www.jsonkeeper.com', "www.pexels.com", "images.pexels.com", "uploads.turbologo.com", "www.svgrepo.com" ]
   }
   };
