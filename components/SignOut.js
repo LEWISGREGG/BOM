8faf95c12a7825a881087f21a7c832ec425b0049
SERG <40707060+LEWISGREGG@users.noreply.github.com>
2024-04-17 19:01:07 -0400

    Add files via upload

--- components/SignOut.js
+++ components/SignOut.js
@@ -20,15 +20,12 @@ function SignOut() {
 <Image src="https://www.svgrepo.com/show/420135/cable-device-electronic.svg" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
 </div>
 
-<div onClick={() => router.push('/') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
-<Image src="https://www.svgrepo.com/show/420100/device-electronic-machine-19.svg" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
+<div onClick={() => router.push('/SoilClassification') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
+<Image src="https://www.svgrepo.com/show/235213/robot-ai.svg" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
 </div>
 
 
-
-
-
-<div onClick={() => router.push('/Hoa') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
+<div onClick={() => router.push('/HOA') } className="relative flex items-center col-span-5 h-14 cursor-pointer my-auto">
 <Image src="https://www.svgrepo.com/show/420104/device-electronic-machine-21.svg" alt="" layout="fill" objectFit='contain' objectPosition="left" className='rounded'/>
 </div>
             <button className='text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900' 
